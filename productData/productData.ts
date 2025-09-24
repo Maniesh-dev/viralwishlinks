50951406f65631780c63298ecd2f15590ee32fd6
Maniesh Sanwal <manieshsanwal.dev@gmail.com>
2025-09-24 16:44:41 +0530

    products done

--- productData/productData.ts
+++ productData/productData.ts
@@ -3,70 +3,70 @@ export const productData = [
     id: 1,
     name: "Women's Pure Rayon Cotton Real Mirror Work Choli With Blouse Peice",
     category: "clothing",
-    image: "/ProductImages/10.jpg",
+    image: "/ProductImages/1.jpg",
     link: "https://amzn.to/4n630sU"
   },
   {
     id: 2,
     name: "Trending Womens Ethnic sets with top, Bottom, Koti| Ethnic Wear",
     category: "clothing",
-    image: "/ProductImages/9.jpg",
+    image: "/ProductImages/2.jpg",
     link: "https://amzn.to/4nBM8dC"
   },
   {
     id: 3,
     name: "Women's Pure Rayon Cotton",
     category: "clothing",
-    image: "/ProductImages/8.jpg",
+    image: "/ProductImages/3.jpg",
     link: "https://amzn.to/4mmGr2f"
   },
   {
     id: 4,
     name: "shubhlaa Women's Heavy Silk Embridery Work Multicolor Thread Navratri Special Kuttchi",
     category: "clothing",
-    image: "/ProductImages/7.jpg",
+    image: "/ProductImages/4.jpg",
     link: "https://amzn.to/4mfgZeH"
   },
   {
     id: 5,
     name: "Youthnic Women's Poly Rayon Bandhani Print Alia Cut Peplum Top",
     category: "clothing",
-    image: "/ProductImages/6.jpg",
+    image: "/ProductImages/5.jpg",
     link: "https://amzn.to/3I00tS3"
   },
   {
     id: 6,
     name: "BHARSHIV FASHION Women's Georgette Kurta for Daily Casual Party Festive Navratri Function Comfortable Traditional Ethnic Outfit Stylish Dress",
     category: "clothing",
-    image: "/ProductImages/5.jpg",
+    image: "/ProductImages/6.jpg",
     link: "https://amzn.to/3IbyVZZ"
   },
   {
     id: 7,
     name: "Jay Varudi Creation Women's Tussar Silk Lehenga Choli Set",
     category: "clothing",
-    image: "/ProductImages/4.jpg",
+    image: "/ProductImages/7.jpg",
     link: "https://amzn.to/4noS0r3"
   },
   {
     id: 8,
     name: "EthnicJunction Women's Georgette Bandhani Shoulder Knot Flared Short Kurti",
     category: "clothing",
-    image: "/ProductImages/3.jpg",
+    image: "/ProductImages/8.jpg",
     link: "https://amzn.to/4mtKMAM"
   },
   {
     id: 9,
     name: "DIGITAL SHOPEE Rayon Block Print Flared Anarkali Regular Wear Kurti ",
     category: "clothing",
-    image: "/ProductImages/2.jpg",
+    image: "/ProductImages/9.jpg",
     link: "https://amzn.to/46Aw06l"
   },
   {
     id: 10,
     name: "EthnicJunction Georgette Women Chikan Embroidery Short A-Line Kurti ",
     category: "clothing",
-    image: "/ProductImages/1.jpg",
+    image: "/ProductImages/10.jpg",
     link: "https://amzn.to/4pxkuAj"
   },
 
